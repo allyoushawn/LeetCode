ab9706f591afa1165d4885b67c3e3207e7493ea5
allyoushawn <b99901025@ntu.edu.tw>
2020-02-29 12:42:54 -0500

    update binary search

--- leetcode/algorithms/binary_search.py
+++ leetcode/algorithms/binary_search.py
@@ -3,18 +3,18 @@
 def binary_search(nums, n):
     start = 0
     end = len(nums) - 1
-    while end - start > 1:
-        mid = (end + start) // 2
+    while end >= start:
+        mid = start + (end - start) // 2
+        if nums[mid] == n: return mid
+
         if nums[mid] < n:
-            start = mid
+            start = mid + 1
         elif nums[mid] > n:
-            end = start
-        else:
-            return mid
-    return start
+            end = mid - 1
+    return -1
 
 l1 = [2, 8, 10, 21, 13, 72]
 n = 15
 idx = binary_search(l1, n)
-l1 = l1[:idx+1] + [n] + l1[idx+1:]
-print(l1)
+print('{}, target: {}'.format(l1, n))
+print(idx)
